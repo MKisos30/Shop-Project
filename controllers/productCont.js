13783049523f3b67a657401b92e7f8fb61bbf980
Miriam Kisos <87091443+MKisos30@users.noreply.github.com>
2022-06-23 17:02:22 +0300

    function

--- controllers/productCont.js
+++ controllers/productCont.js
@@ -44,6 +44,12 @@ exports.removeProduct = async (req, res) => {
 //     }
 // }
 
-exports.deleteProduct = async (req, res) => {
-    
+exports.deleteAllProduct = async (req, res) => {
+    try {
+        const products = await Product.removeProduct();
+        res.send(products)
+    } catch (error) {
+        console.log(error)
+        res.send({ok:false, massage: error})
+    }
 }
